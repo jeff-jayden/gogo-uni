3c78d4908dbc98e30439dc7eebc6e69a6453e7f1
Harley Pritt <HarleyPritt@qq.com>
2024-09-19 16:44:41 +0800

    fix: 修改文字

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,7 +10,7 @@ export function request(config={}){
 	} = config
 	
 	url = BASE_URL+url
-	header['access-key'] = "hyh123321#"
+	header['access-key'] = "817990"
 	
 	
 	return new Promise((resolve,reject)=>{		
